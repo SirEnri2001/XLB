fc2e8432dc642401e6af60701700dddc6c4560f4
SirEnri <vlairrrrr@outlook.com>
2024-06-19 16:37:49 +0800

    Implement read_data functions

--- data_generator.py
+++ data_generator.py
@@ -1,6 +1,7 @@
 from src.lattice import LatticeD2Q9
 from src.utils import *
 from termcolor import colored
+from tqdm import tqdm
 import model_settings
 def generate_sim_dataset(diam, ts_start, ts_end, output_offset=0, output_stride=1, init_f=None):
     precision = 'f64/f64'
@@ -80,3 +81,57 @@ def batch_generator_generate_sim_dataset(diam, ts_start, ts_end, output_offset=0
     else:
         return sim.run_batch_generator(ts_end, ts_start, output_offset, output_stride)
 
+def read_data(total_batch):
+    res_data = {
+        'timestep':[],
+        'rho':[],
+        'u':[]
+    }
+    for i in tqdm(range(total_batch)):
+        loaded_data = np.load('./data/batched_ref_data_{}.npz'.format(i))
+        res_data['timestep'].append(loaded_data['timestep'])
+        res_data['rho'].append(loaded_data['rho'])
+        res_data['u'].append(loaded_data['u'])
+    print("concatenating ... ")
+    res_data['timestep'] = np.concatenate(res_data['timestep'], axis=0)
+    res_data['rho'] = np.concatenate(res_data['rho'], axis=0)
+    res_data['u'] = np.concatenate(res_data['u'], axis=0)
+    return res_data
+
+def read_data_and_downsample(total_batch, factor=4):
+    res_data = {
+        'timestep':[],
+        'rho':[],
+        'u':[]
+    }
+    for i in tqdm(range(total_batch)):
+        loaded_data = np.load('./data/batched_ref_data_{}.npz'.format(i))
+        resized_shape = (loaded_data['u'].shape[0], round(loaded_data['u'].shape[1]/factor), round(loaded_data['u'].shape[2]/factor))
+        res_data['timestep'].append(loaded_data['timestep'])
+        res_data['rho'].append(resample_field(loaded_data['rho'], factor, shape=(resized_shape[0], resized_shape[1], resized_shape[2], loaded_data['rho'].shape[3])))
+        res_data['u'].append(resample_field(loaded_data['u'], factor, shape=(resized_shape[0], resized_shape[1], resized_shape[2], loaded_data['u'].shape[3])))
+    print("concatenating ... ")
+    res_data['timestep'] = np.concatenate(res_data['timestep'], axis=0)
+    res_data['rho'] = np.concatenate(res_data['rho'], axis=0)
+    res_data['u'] = np.concatenate(res_data['u'], axis=0)
+    return res_data
+
+def generate_sim_dataset_and_save(diam, ts_start, ts_end, output_offset=0, output_stride=1, init_f=None, file_name="batched_ref_data"):
+    def process_batched_data(batched_data, seq_num):
+        batched_data['timestep'] = np.array(batched_data['timestep'])
+        batched_data['u'] = np.stack(batched_data['u'], axis=0)
+        batched_data['rho'] = np.stack(batched_data['rho'], axis=0)
+        np.savez_compressed("./data/{}_{}.npz".format(file_name, seq_num), timestep=batched_data['timestep'], u=batched_data['u'], rho=batched_data['rho'])
+        print("Seq {} saved".format(seq_num))
+    from concurrent.futures import ThreadPoolExecutor
+    with ThreadPoolExecutor(max_workers=24) as executor:
+        seq = 0
+        tasks = []
+        for batched_data in batch_generator_generate_sim_dataset(diam, ts_start, ts_end, output_offset, output_stride, init_f):
+            tasks.append(executor.submit(process_batched_data, batched_data, seq))
+            seq += 1
+            if len(tasks) == 10:
+                print("Finishing tasks...")
+                for task in tqdm(tasks):
+                    task.result()
+                tasks = []
