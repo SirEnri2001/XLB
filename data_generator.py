83ecb885cc579fad4d7082230250a2287560ce26
SirEnri <vlairrrrr@outlook.com>
2024-06-20 14:25:28 +0800

    Refactor data_generator.py and batch_read from disk

--- data_generator.py
+++ data_generator.py
@@ -3,16 +3,13 @@ from src.utils import *
 from termcolor import colored
 from tqdm import tqdm
 import model_settings
-def generate_sim_dataset(diam, ts_start, ts_end, output_offset=0, output_stride=1, init_f=None):
-    precision = 'f64/f64'
-    # diam_list = [10, 20, 30, 40, 60, 80]
-    scale_factor = 2
-    prescribed_vel = 0.003 * scale_factor
-    lattice = LatticeD2Q9(precision)
 
+def instantiate_simulator(diam, with_cnn_correction=False, transfer_output=True, quiet=False):
+    precision = 'f64/f64'
+    prescribed_vel = 0.006
+    lattice = LatticeD2Q9(precision)
     nx = int(22 * diam)
     ny = int(4.1 * diam)
-
     Re = 100.0
     visc = prescribed_vel * diam / Re
     omega = 1.0 / (3. * visc + 0.5)
@@ -24,14 +21,24 @@ def generate_sim_dataset(diam, ts_start, ts_end, output_offset=0, output_stride=
         'nz': 0,
         'precision': precision,
         'diam': diam,
+        'transfer_output': transfer_output,
+        'quiet': quiet,
         'return_fpost': True  # Need to retain fpost-collision for computation of lift and drag
     }
-    # characteristic time
+    if with_cnn_correction:
+        return model_settings.CylinderForce(**kwargs)
+    else:
+        return model_settings.Cylinder(**kwargs)
+
+def generate_sim_dataset(diam, ts_start, ts_end, output_offset=0, output_stride=1, init_f=None):
+    nx = int(22 * diam)
+    ny = int(4.1 * diam)
+    prescribed_vel = 0.006
     tc = prescribed_vel / diam
     if ts_end < int(100 // tc):
         print(colored("WARNING: timestep_end is too small, Karman flow may not appear. Recommend value is {}".format(
             int(100 // tc)), "red"))
-    sim = model_settings.Cylinder(**kwargs)
+    sim = instantiate_simulator(diam)
     if init_f is not None:
         loaded_data = np.load('./data/init_frame.npz')
         init_f = resample_field(loaded_data['f'], shape=(nx, ny, loaded_data['f'].shape[2]))
@@ -46,34 +53,14 @@ def generate_sim_dataset(diam, ts_start, ts_end, output_offset=0, output_stride=
     return generated_data
 
 def batch_generator_generate_sim_dataset(diam, ts_start, ts_end, output_offset=0, output_stride=1, init_f=None):
-    precision = 'f64/f64'
-    # diam_list = [10, 20, 30, 40, 60, 80]
-    scale_factor = 2
-    prescribed_vel = 0.003 * scale_factor
-    lattice = LatticeD2Q9(precision)
-
     nx = int(22 * diam)
     ny = int(4.1 * diam)
-
-    Re = 100.0
-    visc = prescribed_vel * 40 / Re
-    omega = 1.0 / (3. * visc + 0.5)
-    kwargs = {
-        'lattice': lattice,
-        'omega': omega,
-        'nx': nx,
-        'ny': ny,
-        'nz': 0,
-        'precision': precision,
-        'diam': diam,
-        'return_fpost': True  # Need to retain fpost-collision for computation of lift and drag
-    }
-    # characteristic time
+    prescribed_vel = 0.006
     tc = prescribed_vel / diam
     if ts_end < int(100 // tc):
         print(colored("WARNING: timestep_end is too small, Karman flow may not appear. Recommend value is {}".format(
             int(100 // tc)), "red"))
-    sim = model_settings.Cylinder(**kwargs)
+    sim = instantiate_simulator(diam)
     if init_f is not None:
         loaded_data = np.load('./data/init_frame.npz')
         init_f = resample_field(loaded_data['f'], shape=(nx, ny, loaded_data['f'].shape[2]))
@@ -99,39 +86,50 @@ def read_data(total_batch):
     return res_data
 
 def read_data_and_downsample(total_batch, factor=4):
+    def batch_read(total_batch):
+        from concurrent.futures import ThreadPoolExecutor
+        tasks = []
+        with ThreadPoolExecutor(max_workers=24) as executor:
+            for i in range(total_batch):
+                tasks.append(executor.submit(np.load, './data/batched_ref_data_{}.npz'.format(i)))
+            for task in tqdm(tasks):
+                yield task.result()
     res_data = {
         'timestep':[],
-        'rho':[],
-        'u':[]
+        'u':[],
+        'f_poststreaming':[]
     }
-    for i in tqdm(range(total_batch)):
-        loaded_data = np.load('./data/batched_ref_data_{}.npz'.format(i))
+    for loaded_data in batch_read(total_batch):
         resized_shape = (loaded_data['u'].shape[0], round(loaded_data['u'].shape[1]/factor), round(loaded_data['u'].shape[2]/factor))
         res_data['timestep'].append(loaded_data['timestep'])
-        res_data['rho'].append(resample_field(loaded_data['rho'], factor, shape=(resized_shape[0], resized_shape[1], resized_shape[2], loaded_data['rho'].shape[3])))
         res_data['u'].append(resample_field(loaded_data['u'], factor, shape=(resized_shape[0], resized_shape[1], resized_shape[2], loaded_data['u'].shape[3])))
+        res_data['f_poststreaming'].append(resample_field(loaded_data['f_poststreaming'], factor, shape=(resized_shape[0], resized_shape[1], resized_shape[2], loaded_data['f_poststreaming'].shape[3])))
     print("concatenating ... ")
     res_data['timestep'] = np.concatenate(res_data['timestep'], axis=0)
-    res_data['rho'] = np.concatenate(res_data['rho'], axis=0)
     res_data['u'] = np.concatenate(res_data['u'], axis=0)
+    res_data['f_poststreaming'] = np.concatenate(res_data['f_poststreaming'], axis=0)
     return res_data
 
 def generate_sim_dataset_and_save(diam, ts_start, ts_end, output_offset=0, output_stride=1, init_f=None, file_name="batched_ref_data"):
-    def process_batched_data(batched_data, seq_num):
+    def process_batched_data(batched_data, seq_num, pbar):
         batched_data['timestep'] = np.array(batched_data['timestep'])
         batched_data['u'] = np.stack(batched_data['u'], axis=0)
         batched_data['rho'] = np.stack(batched_data['rho'], axis=0)
-        np.savez_compressed("./data/{}_{}.npz".format(file_name, seq_num), timestep=batched_data['timestep'], u=batched_data['u'], rho=batched_data['rho'])
-        print("Seq {} saved".format(seq_num))
+        np.savez_compressed("./data/{}_{}.npz".format(file_name, seq_num), timestep=batched_data['timestep'], u=batched_data['u'], f_poststreaming=batched_data['f_poststreaming'])
+        pbar.set_description("Seq {} saved".format(seq_num))
     from concurrent.futures import ThreadPoolExecutor
     with ThreadPoolExecutor(max_workers=24) as executor:
         seq = 0
         tasks = []
-        for batched_data in batch_generator_generate_sim_dataset(diam, ts_start, ts_end, output_offset, output_stride, init_f):
-            tasks.append(executor.submit(process_batched_data, batched_data, seq))
+        generator = batch_generator_generate_sim_dataset(diam, ts_start, ts_end, output_offset, output_stride, init_f)
+        pbar = next(generator)
+        for batched_data in generator:
+            tasks.append(executor.submit(process_batched_data, batched_data, seq, pbar))
             seq += 1
-            if len(tasks) == 10:
-                print("Finishing tasks...")
-                for task in tqdm(tasks):
-                    task.result()
-                tasks = []
+            while len(tasks) >= 24:
+                for task in tasks:
+                    if task.done():
+                        tasks.remove(task)
+        print("Finishing batch tasks ... ")
+        for task in tqdm(tasks):
+            task.result()
